05eabc0af15c3a957421f8e877d591f3141210c4
YEOMHYERA <hyera101511@gmail.com>
2023-03-12 13:06:58 +0900

    feature: group create
    화면 구조 변경

--- src/groupCreate/index.js
+++ src/groupCreate/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import groupStyle from './style';
+import { HiUserCircle } from "react-icons/hi";
 
 const Group = () => {
     const gStyle = groupStyle();
@@ -7,14 +8,16 @@ const Group = () => {
     return(
         <div>
             <div className={gStyle.head}>
-                WhiteBoard
+                <span className={gStyle.wb1}>WhiteBoard</span>
+                <span className={gStyle.wbIcon}><HiUserCircle/></span>
+                <span className={gStyle.wb2}>내프로젝트</span>
             </div>
             <div>
                 <h1 className={gStyle.myPJ}>내 프로젝트</h1>
-                <div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                <div className={gStyle.PJP}>
+                    <div className={gStyle.PJ}>A</div>
+                    <div className={gStyle.PJ}>B</div>
+                    <div className={gStyle.PJ}>C</div>
                 </div>
             </div>
 
--- src/groupCreate/style.js
+++ src/groupCreate/style.js
@@ -2,15 +2,59 @@ import { makeStyles } from "@material-ui/core";
 
 const groupStyle = makeStyles((theme)=>({
     head:{
-        height: '70px',
+        height: '50px',
         background: 'FFFFFF',
-        border:'1px solid #000000'
+        border:'1px solid gray'
+    },
+
+    wb1:{
+        margin:'10px',
+        marginLeft:'20px',
+        float:'left',
+        display: 'flex',
+        color:'#c7d2f9',
+        fontSize:'30px'
+
+    },
+
+    wb2:{
+        margin:'10px',
+        float:'right',
+        display:'flex',
+        fontSize:'20px'
+    },
+
+    wbIcon:{
+        margin:'10px',
+        float:'right',
+        display:'flex',
+        fontSize:'20px'
     },
 
     myPJ:{
         fontSize:'30px',
         margin:'25px',
-    }
+    },
+
+    PJP:{
+        textAlign: 'center',
+        height:'auto',
+        
+    },
+
+    PJ:{
+        display:'inline',
+        padding:'70px 120px',
+        backgroundColor:'#ebeef6',
+        border:'1px solid gray',
+        borderRadius:'10px',
+        margin:'50px',
+
+    },
+
+
+
+
 }))
   
   export default groupStyle
