65939ac787f41a98268e4c9c2c12b8ebcf64b572
Dabin Jeong <allempti@gmail.com>
2023-05-28 19:57:39 +0900

    Style Home UI
    메인화면 UI

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import girl from './icon/girl.png';
+import circle from './icon/circle.png';
+import chat from './icon/chat.png';
+import compass from './icon/compass.png';
+import heart from './icon/heart.png';
 import "./style.css";
 
 function Main() {
@@ -11,8 +15,16 @@ function Main() {
           <h1 className="title-line">Flow Meet과 함께</h1>
         </div>
         <button className="start-btn">시작하기</button>
-        <div className="fly">
-          <img src={girl} className="girl" alt=""></img>
+        <div className="top-figure-wrap">
+          <div className="fly">
+            <img src={girl} className="girl" alt=""></img>
+            <img src={compass} className="compass" alt=""></img>
+            <img src={heart} className="heart" alt=""></img>
+            <img src={circle} className="circle-1" alt=""></img>
+            <img src={circle} className="circle-2" alt=""></img>
+            <img src={circle} className="circle-3" alt=""></img>
+            <img src={chat} className="chat" alt=""></img>
+          </div>
         </div>
         <div className="wave-box">
           <div className="wave -one"></div>
