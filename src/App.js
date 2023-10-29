420a2c325e1a4c1be937d42bde73a640a0ca09b2
Dabin Jeong <allempti@gmail.com>
2023-10-29 22:24:43 +0900

    feat: 보드 리스트 및 그룹 리스트 수정
    - boardlist 경로에 id 추가
    - 생성된 그룹이 없을 때 'no group' 출력

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './layouts/Header';
-import Footer from './layouts/Footer';
-import Main from './pages/Main';
-import Group from './pages/Group';
-import BoardList from './pages/BoardList';
-import Board from './pages/board/Board';
-import MinutesList from './pages/minutes/MinutesList';
-import MinutesDetails from './pages/minutes/detail/MinutesDetails';
-import CollaborationComponent from './pages/minutes/edit/CollaborativeEditor';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./layouts/Header";
+import Footer from "./layouts/Footer";
+import Main from "./pages/Main";
+import Group from "./pages/Group";
+import BoardList from "./pages/BoardList";
+import Board from "./pages/board/Board";
+import MinutesList from "./pages/minutes/MinutesList";
+import MinutesDetails from "./pages/minutes/detail/MinutesDetails";
+import CollaborationComponent from "./pages/minutes/edit/CollaborativeEditor";
 
 const rootElement = document.getElementById("root");
 render(<Board />, rootElement);
@@ -22,13 +22,15 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/group" element={<Group />} />
-          <Route path="/boardlist" element={<BoardList />} />
+          <Route path="/boardlist/:id" element={<BoardList />} />
           <Route path="/board" element={<Board />} />
 
           <Route path="/minutes" element={<MinutesList />} />
           <Route path="/minutes/:id" element={<MinutesDetails />} />
-          <Route path="/minutes/:id/edit" element={<CollaborationComponent />} />
-
+          <Route
+            path="/minutes/:id/edit"
+            element={<CollaborationComponent />}
+          />
         </Routes>
         <Footer />
       </div>
